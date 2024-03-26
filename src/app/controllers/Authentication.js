c0df921eeea689823cb764c37fd5de06d6dca240
PhanTrongHau <haupk2003@gmail.com>
2024-03-26 13:34:08 +0700

    change variable role to isAdmin for user

--- src/app/controllers/Authentication.js
+++ src/app/controllers/Authentication.js
@@ -1,5 +1,5 @@
 export const restrictToAdmins = (req, res, next) => {
-  if (req.session.user?.role !== "admin") {
+  if (!req.session.user?.isAdmin) {
     return res
       .status(401)
       .send({ message: "Authorization header required", status: 401 });
