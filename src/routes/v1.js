1aec2a7047403d8561b3bef430eb44fee1a0e5e5
PhanTrongHau <haupk2003@gmail.com>
2024-04-01 15:18:07 +0700

    create method get data and post rule admin for account

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ProductController from "../app/controllers/Product.js";
 import MerchController from "../app/controllers/Merch.js";
+import AccountController from "../app/controllers/Account.js";
 import { restrictToAdmins } from "../app/controllers/Authentication.js";
 const router = express.Router();
 
@@ -17,9 +18,13 @@ router.delete(
 
 // router merch shop
 router.get("/merch", MerchController.getAll);
-router.post("/merch",restrictToAdmins, MerchController.post);
+router.post("/merch", restrictToAdmins, MerchController.post);
 router.get("/merch/:merchId", MerchController.getId);
-router.put("/merch/:merchId",restrictToAdmins, MerchController.put);
-router.delete("/merch/:merchId",restrictToAdmins, MerchController.delete);
+router.put("/merch/:merchId", restrictToAdmins, MerchController.put);
+router.delete("/merch/:merchId", restrictToAdmins, MerchController.delete);
+
+// router users
+router.get("/accounts", AccountController.getAll);
+router.post("/accounts/:email", AccountController.post);
 
 export default router;
