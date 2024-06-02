ef33b91e3d0fe6bcc779c21e0706f73ae54dc554
PhanTrongHau <haupk2003@gmail.com>
2024-06-02 21:43:37 +0700

    change post method to put method

--- src/app/controllers/Account.js
+++ src/app/controllers/Account.js
@@ -21,13 +21,15 @@ class AccountController {
   }
 
   // [POST] /login
-  async post(req, res) {
+  async put(req, res) {
     const email = req.params.email;
-
+    const { isAdmin } = req.body;
     try {
-      await Accounts.findOne({ email }).updateOne({
-        isAdmin: true,
+      const result = await Accounts.findOne({ email }).updateOne({
+        isAdmin: isAdmin,
       });
+      console.log({ result });
+
       res.json({ status: 200, message: "Update successfully" });
     } catch (error) {
       res.json({ status: 500, message: error });
