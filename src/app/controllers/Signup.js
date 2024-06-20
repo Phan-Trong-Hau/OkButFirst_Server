653461ebb3d9687cabaea36a628f1cc478cd4198
PhanTrongHau <haupk2003@gmail.com>
2024-06-20 16:32:33 +0700

    create nodemailer config file

--- src/app/controllers/Signup.js
+++ src/app/controllers/Signup.js
@@ -1,29 +1,9 @@
 import bcrypt from "bcrypt";
-import nodemailer from "nodemailer";
-import { v4 } from "uuid";
 import "dotenv/config";
 
 import accounts from "../models/account.js";
-import verification from "../models/verification.js";
 import { createAccountValidate } from "../helper/validation.js";
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASS,
-  },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Login for Email successfully!!!");
-  }
-});
+import { sendVerificationEmail } from "../../config/nodemailer/index.js";
 
 const bcryptData = async (data) => {
   try {
@@ -36,62 +16,6 @@ const bcryptData = async (data) => {
       message: "Data encryption error ;<",
     });
   }
-};
-
-const sendVerificationEmail = ({ _id, email, username }, res) => {
-  const currentUrl = process.env.URL_BACKEND;
-  const uniqueString = v4() + _id;
-
-  const mailOptions = {
-    from: `OK BUT FIRST COFFEE ${process.env.AUTH_EMAIL}`,
-    to: email,
-    subject: "Verify Your Email",
-    html: `
-        <h3>Dear ${username}</h3>
-        <p>Verify your email address to complete the signup and login into your account.</p>
-        <p>This link <b>expires in 15 minute</b>.</p>
-        <h4>Press <a href=${
-          currentUrl + "/auth/user/verify/" + _id + "/" + uniqueString
-        }>here</a> to proceed.</h4>`,
-  };
-
-  bcryptData(uniqueString)
-    .then((data) => {
-      verification
-        .create({
-          userId: _id,
-          uniqueString: data,
-          expiresAt: Date.now() + 900000,
-        })
-        .then((data) => {
-          transporter
-            .sendMail(mailOptions)
-            .then((data) =>
-              res.send(
-                "You have successfully registered an account. Please check your mailbox for email confirmation!!!",
-              ),
-            )
-            .catch((err) =>
-              res.json({
-                status: 500,
-                message: "There was an error sending the verification email ;<",
-              }),
-            );
-        })
-        .catch((err) =>
-          res.json({
-            status: 500,
-            message:
-              "There was an error while generating email authentication data ;<",
-          }),
-        );
-    })
-    .catch((err) =>
-      res.json({
-        status: 500,
-        message: "There was an error in the translation process ;<",
-      }),
-    );
 };
 
 class SignupController {
